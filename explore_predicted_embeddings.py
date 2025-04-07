dbcfe8219cf35863caaa19c087736d829d581c26
Andrzej Szablewski <as3623@cam.ac.uk>
2025-04-07 22:18:05 +0100

    Minor changes in the exploration code. Added torch.no_grad() context manager.
    Signed-off-by: Andrzej Szablewski <as3623@cam.ac.uk>

--- explore_predicted_embeddings.py
+++ explore_predicted_embeddings.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from embedding_hypernetwork.embeddings import (
     load_llama_embedding_layer,
-    split_embedding,
+    split_embedding_idx,
 )
 from embedding_hypernetwork.rnn_model import DynamicRNNModel
 
@@ -31,12 +31,16 @@ def experiment_digits(embedding_layer, tokenizer, model):
     vocab_set = set(tokenizer.get_vocab().keys())
     splits = []
     for idx in input_ids:
-        splits.append(split_embedding(idx[0], tokenizer, vocab_set, embedding_layer))
+        splits.append(
+            split_embedding_idx(idx[0], tokenizer, vocab_set, embedding_layer)
+        )
 
     lengths = [len(s) for s in splits]
     padded_sequences = torch.nn.utils.rnn.pad_sequence(splits, batch_first=True)
 
-    predicted_embeddings = model(padded_sequences, lengths)
+    model.eval()
+    with torch.no_grad():
+        predicted_embeddings = model(padded_sequences, lengths)
 
     combined_embeddings = torch.cat(
         [embeddings, predicted_embeddings], dim=0
@@ -127,7 +131,7 @@ def experiment_embedding_vocab(embedding_layer, tokenizer, model, dataset_split_
     splits = []
     for i, idx in enumerate(input_ids):
         if (
-            split := split_embedding(idx[0], tokenizer, vocab_set, embedding_layer)
+            split := split_embedding_idx(idx[0], tokenizer, vocab_set, embedding_layer)
         ) is not None:
             splits.append(split)
         else:
@@ -143,7 +147,9 @@ def experiment_embedding_vocab(embedding_layer, tokenizer, model, dataset_split_
     lengths = [len(s) for s in splits]
     padded_sequences = torch.nn.utils.rnn.pad_sequence(splits, batch_first=True)
 
-    predicted_embeddings = model(padded_sequences, lengths)
+    model.eval()
+    with torch.no_grad():
+        predicted_embeddings = model(padded_sequences, lengths)
 
     combined_embeddings = torch.cat(
         [embeddings, predicted_embeddings], dim=0
@@ -208,7 +214,7 @@ def experiment_embedding_vocab(embedding_layer, tokenizer, model, dataset_split_
 def main():
     # Load the embedding layer
     model_dir = "/nfs-share/as3623/models/Llama-3.2-1B/"
-    rnn_model_dir = "/nfs-share/as3623/projects/L65-nat/NAT/checkpoints-20250406_213652/best_model.pt"
+    rnn_model_dir = "/nfs-share/as3623/projects/L65-nat/NAT/checkpoints-20250407_145814/final_model.pt"
     dataset_split_file = "./test_indices.txt"
 
     np.random.seed(42)
@@ -220,14 +226,12 @@ def main():
 
     rnn_model = DynamicRNNModel(
         input_dim=2048,
-        hidden_dim=512,
+        hidden_dim=1024,
         output_dim=2048,
-        num_layers=2,
+        num_layers=4,
         rnn_type="lstm",
         dropout=0.1,
     ).to(device)
-
-    rnn_model.eval()
 
     # load the model state dict
     rnn_model.load_state_dict(
@@ -238,7 +242,7 @@ def main():
         )["model_state_dict"]
     )
 
-    experiment_digits(embedding_layer, tokenizer, rnn_model)
+    # experiment_digits(embedding_layer, tokenizer, rnn_model)
     experiment_embedding_vocab(
         embedding_layer, tokenizer, rnn_model, dataset_split_file
     )
